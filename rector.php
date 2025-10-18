e81718da531529c830a3ad399621ea37c96cb234
Tuhin Rana Mondal <aimlesstuhin@gmail.com>
2025-10-18 21:27:34 +0530

    Setup Project

---
+++ rector.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
+use Rector\CodeQuality\Rector\FuncCall\CompactToVariablesRector;
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+use RectorLaravel\Set\LaravelSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap',
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/resources',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ])
+    ->withPhpSets(php84: true)
+    ->withPreparedSets(deadCode: true, typeDeclarations: true, earlyReturn: true)
+    ->withSets([
+        LaravelSetList::LARAVEL_120,
+    ])
+    ->withRules([
+        InlineConstructorDefaultToPropertyRector::class,
+        DeclareStrictTypesRector::class,
+        CompactToVariablesRector::class,
+    ]);;
