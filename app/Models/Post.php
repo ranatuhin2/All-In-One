1c113b82119c31a73523f62f7af1eb8e10d1d443
Tuhin <tuhin.thinktoshare@gmail.com>
2025-10-25 13:19:21 +0000

    Added User and Post Resource

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,11 +4,25 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Enums\PostStatusType;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
     use HasFactory;
+
+    protected function casts(): array
+    {
+        return [
+            'status' => PostStatusType::class
+        ];
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
